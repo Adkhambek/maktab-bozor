6e340fa3ea2ceb52ac70881457a592133f3b162c
Adham <muzaffarov.adham@gmail.com>
2022-02-01 09:18:54 +0500

    credentials

--- include/config.php
+++ include/config.php
@@ -1,15 +1,15 @@
 <?php
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "heaven8_mbozor");
-define("DB_PASS", "sHy&22bn");
-define("DB_NAME", "heaven8_mbozor");
+define("DB_USER", "<username>");
+define("DB_PASS", "<password>");
+define("DB_NAME", "<database name>");
 
 // Others
-define("BOT_TOKEN", "1249740775:AAE9hvgSu7yVOGl1jI5T8ziK3Qs_InxjlS8");
-define("PHONE_NUMBER", "+998998828299");
-define("USERNAME", "@askmaktabbozor");
-define("ROOT_PATH", "https://skillsoft.uz/mbozor/photos/");
+define("BOT_TOKEN", "<TOKEN>");
+define("PHONE_NUMBER", "+998");
+define("USERNAME", "@");
+define("ROOT_PATH", "https://example.com/mbozor/");
 define("DATA_TIME", [24,1,2,3,4,5,6,7]);
 define("ADMIN_CHAT_ID", [
     516468111,
